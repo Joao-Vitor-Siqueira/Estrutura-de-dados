38cfd07888fde51a2bd9c2b0a7f6edec29855640
João Vitor Siqueira <jvitorsiqueira99@gmail.com>
2023-08-19 12:56:15 -0300

    Pilha e fila

---
+++ DataStructures/src/MyQueue.java
@@ -0,0 +1,73 @@
+public class MyQueue <T>{
+    private T[] data;
+    private int head = -1;
+    private int tail = -1;
+
+    private int size;
+
+    public MyQueue(int size){
+        this.size = size;
+        data = (T[]) new Object[size];
+    }
+    public void add(T item){
+        if(isFull()){
+            System.out.println("A fila está cheia!");
+        }
+        else {
+            if(head == -1){
+                head = 0;
+            }
+            tail = (tail + 1) % size;
+            data[tail] = item;
+        }
+    }
+    public T remove(){
+        T element;
+        if(isEmpty()){
+            System.out.println("A fila está vazia!");
+            return null;
+        }
+        element = data[head];
+        data[head] = null;
+        if(head == tail){
+            head = -1;
+            tail = -1;
+        }
+        else {
+            head = (head + 1) % size;
+        }
+        return element;
+    }
+
+    public boolean isEmpty(){
+        return head == -1;
+    }
+
+    public boolean isFull(){
+        if(head == 0 && tail == size - 1){
+            return true;
+        }
+        return head == tail + 1;
+    }
+
+    public void clear(){
+        for (int i = 0; i < size; i++) {
+            data[i] = null;
+        }
+        head = -1;
+        tail = -1;
+    }
+
+    @Override
+    public String toString() {
+        String queue = "[";
+        for(int i = 0;i < size; i ++){
+            queue += data[i];
+            if(i != data.length - 1){
+                queue += ",";
+            }
+        }
+        queue += "]" + "  Head: " + head + "   Tail:" + tail;
+        return queue;
+    }
+}
---
+++ DataStructures/src/MyStack.java
@@ -0,0 +1,62 @@
+public class MyStack <T>{
+    private T[] data;
+    private int top;
+    private int size;
+
+    public MyStack(int size){
+        this.size = size;
+        data = (T[]) new Object[size];
+        top = -1;
+    }
+
+    public void push(T item){
+        if (isFull()){
+            System.out.println("A pilha está cheia!");
+        }
+        else {
+            top ++;
+            data[top] = item;
+        }
+    }
+
+    public T pop(){
+        T element;
+        if(isEmpty()){
+            System.out.println("A pilha está vazia!");
+            return null;
+        }
+        else {
+            element = data[top];
+            data[top] = null;
+            top --;
+            return element;
+        }
+    }
+    public boolean isFull(){
+        return top == size - 1;
+    }
+
+    public boolean isEmpty(){
+        return top == -1;
+    }
+
+    public void clear(){
+        for (int i = 0; i < size; i++) {
+            data[i] = null;
+        }
+        top = -1;
+    }
+
+    @Override
+    public String toString() {
+        String stack = "[";
+        for(int i = 0;i < size; i ++){
+            stack += data[i];
+            if(i != data.length - 1){
+                stack += ",";
+            }
+        }
+        stack += "]" + "  Top: " + top;
+        return stack;
+    }
+}
